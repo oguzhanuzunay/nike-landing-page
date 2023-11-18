bb1e9ea9af773498ad5aa6a20ab7605030b8dcd0
Oguzhan <oguzhanzny@gmail.com>
2023-11-18 05:30:51 +0300

    change: base config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nike-landing-page/",
+  base: "/projects/nike-landing-page/",
 });
 
