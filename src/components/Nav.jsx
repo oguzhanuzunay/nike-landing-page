719f22b7e228dc9c96c2fc3a6495c3d93e927015
Oguzhan <oguzhanzny@gmail.com>
2023-12-01 04:51:08 +0300

    add: animation and load effect

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,31 @@
-import { hamburger } from "../assets/icons";
-import { headerLogo } from "../assets/images";
-import { navLinks } from "../constants";
+import { hamburger } from '../assets/icons';
+import { headerLogo } from '../assets/images';
+import { navLinks } from '../constants';
 
 const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
-        <a href="/">
-          <img src={headerLogo} alt="logo" width={130} height={29} />
+        <a
+          href="/"
+          className="logo-link group"
+        >
+          <img
+            src={headerLogo}
+            alt="logo"
+            width={130}
+            height={29}
+            className="m-0 w-[129px] h-[29px] logo transition-transform transform group-hover:scale-105"
+          />
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
-            <li key={item.label} className="text-sm">
+            <li
+              key={item.label}
+              className="text-sm"
+            >
               <a
-                className="font-montserrat leading-normal text-lg text-slate-gray"
+                className="font-montserrat leading-normal text-lg text-slate-gray hover-underline-expand"
                 href={item.href}
               >
                 {item.label}
@@ -21,8 +33,28 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-        <div className="lg:hidden block">
-          <img src={hamburger} width={25} height={25} />
+        <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+          <a
+            href="/"
+            className="transition-colors hover:text-[#FF4040]"
+          >
+            Sign in
+          </a>
+          <span>/</span>
+          <a
+            href="/"
+            className="transition-colors hover:text-[#FF4040]"
+          >
+            Explore now
+          </a>
+        </div>
+        <div className="hidden max-lg:block">
+          <img
+            src={hamburger}
+            alt="hamburger icon"
+            width={25}
+            height={25}
+          />
         </div>
       </nav>
     </header>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,60 +1,74 @@
-import { arrowRight } from "../assets/icons";
-import { bigShoe1 } from "../assets/images";
-import Button from "../components/Button";
-import ShoeCard from "../components/ShoeCard";
-import { shoes, statistics } from "../constants";
+import { arrowRight } from '../assets/icons';
+import { bigShoe1 } from '../assets/images';
+import Button from '../components/Button';
+import ShoeCard from '../components/ShoeCard';
+import { shoes, statistics } from '../constants';
 
-import { useState } from "react";
+import { useEffect, useState } from 'react';
 
 const Hero = () => {
   const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setLoaded(true);
+    }, 1000); // Adjust the delay time (in milliseconds) as needed
+
+    return () => clearTimeout(delay);
+  }, []);
 
   return (
     <section
       id="home"
-      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+      className={`w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container ${
+        loaded ? 'opacity-1 transition-opacity duration-1000' : 'opacity-0'
+      }`}
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28">
-        <p className="text-xl font-montserrat text-coral-red">
-          Our Summer collections
-        </p>
+        <p className="text-xl font-montserrat text-coral-red">Our Summer collections</p>
 
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The New Arrival
           </span>
           <br />
-          <span className="text-coral-red inline-block mt-3 nike-animation">
-            Nike&nbsp;
-          </span>
+          <span className="text-coral-red inline-block mt-3 nike-animation">Nike&nbsp;</span>
           Shoes
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
+          Discover stylish Nike arrivals, quality comfort, and innovation for your active life.
         </p>
 
-        <Button label="Shop now" iconURL={arrowRight} />
+        <Button
+          label="Shop now"
+          iconURL={arrowRight}
+        />
 
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16 ">
           {statistics.map((stat) => (
-            <div key={stat.label} className="rotate">
+            <div
+              key={stat.label}
+              className="rotate"
+            >
               <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-              <p className="leading-7 font-montserrat text-slate-gray">
-                {stat.label}
-              </p>
+              <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
             </div>
           ))}
         </div>
       </div>
 
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+      <div
+        className={`relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center ${
+          loaded ? 'opacity-1 transition-opacity duration-1000' : 'opacity-0'
+        }`}
+      >
         <img
           src={bigShoeImg}
           alt="shoe colletion"
           width={610}
           height={502}
-          className="object-contain relative z-10"
+          className="object-contain relative z-10 hover:scale-105 transition-transform duration-300 ease-in-out"
         />
 
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6 ">
